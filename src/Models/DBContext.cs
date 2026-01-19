b93a0f7c9104586df38e3d0f26953e10b78ebdd8
Musssty <mustykhoram@icloud.com>
2026-01-19 13:49:03 +1100

    fixed packages issue

---
+++ src/Models/DBContext.cs
@@ -0,0 +1,12 @@
+namespace FollowTheMoney.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class DBContext : DbContext
+{   
+    public DbSet<Organisation> Organisations { get; set; }
+    public DbSet<Asset> Assets { get; set; }
+    public DbSet<Donation> Donations { get; set; }
+    public DbSet<PoliticalParty> PoliticalParties { get; set; }
+    public DbSet<Politician> Politicians { get; set; }
+    public DbSet<Stock> Stocks { get; set; }
+}
